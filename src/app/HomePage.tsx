c5d08b504d799db304f2c7f3c29b59b0ee65ff3c
karel <kareltrisnanto26@gmail.com>
2024-05-11 11:21:14 +0700

    add: navbar and pwa

--- src/app/HomePage.tsx
+++ src/app/HomePage.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const HomePage = () => {
   return (
-    <div className="w-full h-fit px-8 sm:px-10 lg:px-14 pt-[135px] sm:pt-[150px] lg:pt-[180px] pb-12 sm:pb-16 flex justify-between items-center flex-col-reverse lg:flex-row">
+    <div className="w-full h-fit px-8 sm:px-10 lg:px-14 pt-[135px] sm:pt-[150px] lg:pt-[180px] pb-12 sm:pb-16">
       <div>HomePage</div>
     </div>
   );
