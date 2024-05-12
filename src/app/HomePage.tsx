a703ccfb8527c8789e90bcf7d08f2f35424847fe
dodichakill <dodiherlangga91@gmail.com>
2024-05-12 15:17:26 +0700

    UI home: slice heroHome section

--- src/app/HomePage.tsx
+++ src/app/HomePage.tsx
@@ -1,11 +1,10 @@
-"use client";
-
 import React from "react";
+import HomeHeroSection from "./HomeHeroSection";
 
 const HomePage = () => {
   return (
     <div className="w-full h-fit px-8 sm:px-10 lg:px-14 pt-[135px] sm:pt-[150px] lg:pt-[180px] pb-12 sm:pb-16">
-      <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et magnam officiis eaque maiores quisquam magni atque, accusamus vitae soluta unde amet labore deleniti tempora delectus cum molestias reprehenderit eius impedit cumque aliquid est possimus quas ullam blanditiis? Explicabo minus, dicta vitae expedita minima suscipit ratione cumque at excepturi tempore doloribus! Repellendus unde voluptatibus dolore, at rerum assumenda porro tenetur vel tempora, nulla officiis nemo totam eius reiciendis ipsam iusto perferendis excepturi natus nesciunt. Quod, quia! Necessitatibus fugiat dicta vero inventore error libero enim non id accusantium ducimus repellat, eveniet atque quisquam nam, rerum saepe maiores quibusdam quis vitae culpa praesentium, fuga accusamus. Adipisci eaque pariatur delectus. Facilis error, aliquam illum adipisci eum cupiditate dignissimos fugiat vitae quaerat dolores architecto! Eligendi repellendus quas consequuntur cumque eius reprehenderit? Libero, magnam non aut veritatis quam, consequuntur nihil eius quae, ipsa dolorem cum! Sit mollitia doloribus vitae ducimus temporibus. Beatae quo doloribus eum aliquid magni ipsum iste voluptatem quia consequuntur aperiam, repellendus voluptatibus laudantium repellat animi. In rem amet atque voluptatem enim commodi veritatis, tempore est sequi ipsam ad dolorem recusandae nisi maxime eius totam nihil eos reiciendis provident? Velit quo aliquam dolorum dolore iste, corporis sequi perspiciatis sint eaque provident enim, aspernatur nesciunt?</div>
+      <HomeHeroSection />
     </div>
   );
 };
