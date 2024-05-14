98b46179e5ecb6ee05d33fc8426d98e2be3cb3f9
karel <kareltrisnanto26@gmail.com>
2024-05-14 11:36:38 +0700

    add: about page

--- src/components/custom/WABtn.tsx
+++ src/components/custom/WABtn.tsx
@@ -5,7 +5,7 @@ import { IoLogoWhatsapp } from "react-icons/io";
 const WABtn = () => {
   return (
     <React.Fragment>
-      <Link target="_blank" href={"https://wa.me/6285524632537"} className="inline-block fixed bottom-20 sm:bottom-20 right-5 z-10">
+      <Link target="_blank" href={`https://wa.me/6285524632537?text=${encodeURIComponent("Hello, I Want to Order Tech Titans Service")}`} className="inline-block fixed bottom-20 sm:bottom-20 right-5 z-10">
         <IoLogoWhatsapp className="text-[2.5rem] sm:text-5xl bg-transparent text-emerald-600" />
       </Link>
     </React.Fragment>
